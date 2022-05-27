93cb0639e7829a62be0a637729d73b8164505062
Abhishek <95518276+Abhishek0075@users.noreply.github.com>
2022-05-27 17:45:07 +0530

    Update and rename Main.java to first.java

--- first.java
+++ first.java
@@ -7,7 +7,7 @@ Code, Compile, Run and Debug online from anywhere in world.
 
 *******************************************************************************/
 import java.util.Scanner;
-class Main{
+class first{
     public static void sum(int n1,int n2){
         int sum=n1+n2;
 		System.out.println("The sum = "+sum);
